680dc1b0639e615bf8583e70422d7cf3c29d867f
Legintras <94419432+contreras-jonnigel@users.noreply.github.com>
2024-01-20 19:49:05 +0800

    header

--- src/app/sticky-column/sticky-column.component.ts
+++ src/app/sticky-column/sticky-column.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-sticky-column',
   templateUrl: './sticky-column.component.html',
-  styleUrls: ['./sticky-column.component.css']
+  styleUrls: ['./sticky-column.component.css', '../../../node_modules/lineicons/web-font/lineicons.css']
 })
 export class StickyColumnComponent {
 
